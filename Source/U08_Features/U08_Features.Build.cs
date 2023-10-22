70b464cff960dcc3261637631157f02ab3db3fb9
HanYooTae <han931120@gmail.com>
2023-10-22 22:41:10 +0900

    Project_start

---
+++ Source/U08_Features/U08_Features.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class U08_Features : ModuleRules
+{
+	public U08_Features(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule" });
+    }
+}
