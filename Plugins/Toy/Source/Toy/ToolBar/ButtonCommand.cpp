ebf6dec563ba2960d6a328ef58f2538bb4c14c7c
HanYooTae <han931120@gmail.com>
2023-11-06 15:21:10 +0900

    Spawn Vertex Button

--- Plugins/Toy/Source/Toy/ToolBar/ButtonCommand.cpp
+++ Plugins/Toy/Source/Toy/ToolBar/ButtonCommand.cpp
@@ -1,5 +1,10 @@
 #include "ButtonCommand.h"
 #include "EditorStyleSet.h"
+#include "Interfaces/IMainFrameModule.h"
+#include "DesktopPlatformModule.h"
+#include "IDesktopPlatform.h"
+#include "Serialization/BufferArchive.h"
+#include "DetailPannel/DetailsButton.h"
 
 FButtonCommand::FButtonCommand()
 	: TCommands("ContextName", FText::FromString("ContextDesc"), NAME_None, FEditorStyle::GetStyleSetName())
@@ -29,14 +34,38 @@ void FButtonCommand::RegisterCommands()
 void FButtonCommand::SpawnVertexButton_Pressed()
 {
 	//UE_LOG(LogTemp, Error, L"SpawnVertexButton_Pressed");
-	TArray<const FSlateBrush*> brushes;
+	/*TArray<const FSlateBrush*> brushes;
 	FEditorStyle::GetResources(brushes);
 
 	for (const auto& brush : brushes)
-		UE_LOG(LogTemp, Warning, L"%s", *brush->GetResourceName().ToString());
+		UE_LOG(LogTemp, Warning, L"%s", *brush->GetResourceName().ToString());*/
+
+	IMainFrameModule& mainFrame = FModuleManager::LoadModuleChecked<IMainFrameModule>("MainFrame");
+	void* handle = mainFrame.GetParentWindow()->GetNativeWindow()->GetOSWindowHandle();
+
+	FString path;
+	TArray<FString> fileNames;
+	IDesktopPlatform* platform = FDesktopPlatformModule::Get();
+	platform->OpenFileDialog(handle, "Load", path, "", "Binary File(*.bin)|*.bin", EFileDialogFlags::None, fileNames);
+	if (fileNames.Num() < 1) return;
+
+	FBufferArchive buffer;
+	FFileHelper::LoadFileToArray(buffer, *fileNames[0]);
+
+	// 읽기
+	FMemoryReader reader(buffer, true);
+	buffer.Seek(0);		// 처음부터 다시 읽기
+
+	FVertexData data;
+	reader << data;
+	reader.FlushCache();
+	reader.Close();
+
+	GLog->Logf(L"Vertex Count : %d", data.Positions.Num());
+	GLog->Logf(L"Index Count : %d", data.Indices.Num());
 }
 
 void FButtonCommand::OpenViewerButton_Pressed()
 {
-	UE_LOG(LogTemp, Warning, L"Open Viewr Button is Pressed");
+	UE_LOG(LogTemp, Warning, L"Open Viewer Button is Pressed");
 }
